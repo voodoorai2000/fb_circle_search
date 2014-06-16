c6ce11fa7daf88a5d501dda5786ec3561f3a9102
rgarcia <voodoorai2000@gmail.com>
2014-06-17 00:20:57 +0200

    proof of concept

---
+++ app/helpers/pages_helper.rb
@@ -0,0 +1,7 @@
+module PagesHelper
+
+  def facebook_page_url(id)
+    "https://www.facebook.com/pages/anything/#{id}"
+  end
+
+end
---
+++ app/views/pages/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @page, :id, :name, :fb_page_id, :created_at, :updated_at
