434c2dc0c37b135bd94ac9ae1fde653e01b4eda4
rgarcia <voodoorai2000@gmail.com>
2014-06-21 04:18:36 +0200

    Enabling postgres in development

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140620234504) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "circles", force: true do |t|
     t.string   "name"
     t.string   "fb_id"
