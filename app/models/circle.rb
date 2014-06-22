eb1d6bda1bb6907853e08d7be957f730b520f23a
rgarcia <voodoorai2000@gmail.com>
2014-06-22 02:28:44 +0200

    Add basic scopes to circles

--- app/models/circle.rb
+++ app/models/circle.rb
@@ -1,7 +1,9 @@
 class Circle < ActiveRecord::Base
-  default_scope { order('created_at DESC') }
+  default_scope { order('constituted_at DESC') }
 
   scope :recent,   -> { where revised: false }
-  scope :revised, -> { where revised: true }
-
+  scope :revised,  -> { where revised: true  }
+  scope :groups,    -> { where kind: 'group'  }
+  scope :pages,    -> { where kind: 'page'   }
+  scope :users,     -> { where kind: 'user'   }
 end
