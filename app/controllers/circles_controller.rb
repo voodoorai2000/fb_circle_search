765ca95fd71a0c5a92b9c7f1353a294cf11bdc51
rgarcia <voodoorai2000@gmail.com>
2014-06-24 20:19:04 +0200

    Moving Facebook search functionality to rake task

--- app/controllers/circles_controller.rb
+++ app/controllers/circles_controller.rb
@@ -1,6 +1,3 @@
-require 'facebook_api_group'
-require 'facebook_api_page'
-require 'facebook_api_user'
 class CirclesController < ApplicationController  
   before_filter :authenticate, only: :search
 
@@ -12,27 +9,9 @@ class CirclesController < ApplicationController
     @circles = Circle.revised
   end
 
-  def search
-    FacebookApiGroup.new(current_user).search
-    FacebookApiPage.new.search
-    redirect_to circles_url
-  end
-
   def update
     @circle = Circle.find(params[:id])
     @circle.update(circle_params)
-  end
-
-  def by_andrex
-    File.open("#{Rails.root}/lib/revised_circles.txt", "r") do |f|
-      f.each_line do |name|
-        ["podemos", "podem"].each do |radical|
-          FacebookApiGroup.new(current_user).search("#{radical} #{name}")
-          FacebookApiPage.new.search("#{radical} #{name}")
-        end
-      end
-    end
-    redirect_to circles_url
   end
 
   private
--- lib/tasks/revised.rake
+++ lib/tasks/revised.rake
@@ -1,3 +1,23 @@
+require 'facebook_api'
+require 'facebook_api_group'
+require 'facebook_api_page'
+
+task :search => :environment do
+  FacebookApiGroup.new(User.first).search
+  FacebookApiPage.new.search
+end
+
+task :by_andrex => :environment do
+  File.open("#{Rails.root}/lib/revised_circles.txt", "r") do |f|
+    f.each_line do |name|
+      ["podemos", "podem"].each do |radical|
+        FacebookApiGroup.new(User.first).search("#{radical} #{name}")
+        FacebookApiPage.new.search("#{radical} #{name}")
+      end
+    end
+  end
+end
+
 task :revised => :environment do  
   File.open("#{Rails.root}/lib/revised_circles.txt", "r") do |f|
     f.each_line do |name|
@@ -13,3 +33,4 @@ end
 task :remove_unrelated => :environment do
   Circle.where("lower(unaccent(name)) NOT LIKE ?", "%podem%").map(&:destroy)
 end
+
